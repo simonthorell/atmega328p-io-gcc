9f16cf336244767c7e635aa4a9b5f0a6a07b9cd9
Simon Thorell <kontakt@simonthorell.se>
2024-03-15 13:37:03 +0000

    Timer refactor

--- src/hardware_interfaces/timer.cpp
+++ src/hardware_interfaces/timer.cpp
@@ -20,20 +20,17 @@ void Timer::init() {
         case TIMER0:
             // TODO: Add setup code if timer 0 is needed later...
             break;
-        case TIMER1: // Millis Timer
+        case TIMER1:
+            // Millis Timer
             TCCR1A = 0; // Set entire TCCR1A register to 0
             TCCR1B = 0; // Same for TCCR1B
-
-            // Set up timer at Fcpu/64
             TCCR1B |= (1 << CS11) | (1 << CS10); // Set prescaler to 64
-
-            // Enable timer overflow interrupt
-            TIMSK1 |= (1 << TOIE1);
-
+            TIMSK1 |= (1 << TOIE1); // Enable overflow interrupt
             // Preload timer for 1ms interrupts
             TCNT1 = 0xFF06; // 65536 - 250 = 65286 (0xFF06)
             break;
-        case TIMER2: // Overflow Timer
+        case TIMER2:
+            // Overflow Timer
             TCCR2A = 0;            // Init Timer2A
             TCCR2B = 0;            // Init Timer2B
             TCCR2B |= 0B00000111;  // Prescaler = 1024
